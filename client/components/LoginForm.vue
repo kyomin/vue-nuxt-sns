35392b6158540658c40845874dfad4d7ea6405e0
kyomin <kim031504@naver.com>
2021-07-12 17:52:33 +0900

    폼 검증하기

--- client/components/LoginForm.vue
+++ client/components/LoginForm.vue
@@ -1,11 +1,23 @@
 <template>
     <v-container>
         <v-card>
-            <v-form>
+            <v-form v-model="valid" @submit.prevent="onSubmitForm" ref="form">
                 <v-container>
-                    <v-text-field label="이메일" type="email" required />
-                    <v-text-field label="비밀번호" type="password" required />
-                    <v-btn color="green" type="submit">로그인</v-btn>
+                    <v-text-field 
+                        v-model="email"
+                        :rules="emailRules"
+                        label="이메일" 
+                        type="email" 
+                        required 
+                    />
+                    <v-text-field
+                        v-model="password"
+                        :rules="passwordRules"
+                        label="비밀번호" 
+                        type="password" 
+                        required 
+                    />
+                    <v-btn color="green" type="submit" :disabled="!valid">로그인</v-btn>
                     <v-btn nuxt to="/signup">회원가입</v-btn>
                 </v-container>
             </v-form>
@@ -15,7 +27,25 @@
 
 <script>
 export default {
-    
+    data() {
+        return {
+            valid: false,
+            email: '',
+            password: '',
+            emailRules: [
+                v => !!v || '이메일은 필수입니다.',
+                v => /.+@.+/.test(v) || '이메일 형식이 유효하지 않습니다.'
+            ],
+            passwordRules: [
+                v => !!v || '비밀번호는 필수입니다.'
+            ]
+        }
+    },
+    methods: {
+        onSubmitForm() {
+            this.$refs.form.validate()
+        }
+    }
 };
 </script>
 
--- client/pages/signup.vue
+++ client/pages/signup.vue
@@ -4,13 +4,42 @@
             <v-card>
                 <v-container>
                     <v-subheader>회원가입</v-subheader>
-                    <v-form>
-                        <v-text-field label="이메일" type="email" required />
-                        <v-text-field label="비밀번호" type="password" required />
-                        <v-text-field label="비밀번호 확인" type="password" required />
-                        <v-text-field label="닉네임" type="nickname" required />
-                        <v-checkbox label="Kyomin이 세운 규칙을 잘 준수할 것을 동의합니다" required />
-                        <v-btn color="green" type="submit">가입하기</v-btn>
+                    <v-form v-model="valid" @submit.prevent="onSubmitForm" ref="form">
+                        <v-text-field 
+                            v-model="email" 
+                            label="이메일" 
+                            type="email"
+                            :rules="emailRules"
+                            required 
+                        />
+                        <v-text-field 
+                            v-model="password" 
+                            label="비밀번호" 
+                            type="password"
+                            :rules="passwordRules"
+                            required 
+                        />
+                        <v-text-field 
+                            v-model="passwordCheck" 
+                            label="비밀번호 확인" 
+                            type="password"
+                            :rules="passwordCheckRules"
+                            required 
+                        />
+                        <v-text-field 
+                            v-model="nickname" 
+                            label="닉네임" 
+                            type="nickname"
+                            :rules="nicknameRules"
+                            required 
+                        />
+                        <v-checkbox 
+                            v-model="terms" 
+                            label="Kyomin이 세운 규칙을 잘 준수할 것을 동의합니다"
+                            :rules="[v => !!v || '약관에 동의해야 합니다.']"
+                            required 
+                        />
+                        <v-btn color="green" type="submit" :disabled="!valid">가입하기</v-btn>
                     </v-form>
                 </v-container>
             </v-card>
@@ -22,7 +51,42 @@
 export default {
     data() {
         return {
-            name: 'Nuxt.js'
+            valid: false,   // 전체 validation이 유효하여 회원가입 버튼을 누를 수 있는지?
+            email: '',
+            password: '',
+            passwordCheck: '',
+            nickname: '',
+            terms: false,    // 약관 동의
+            emailRules: [
+                v => !!v || '이메일은 필수입니다.',
+                v => /.+@.+/.test(v) || '이메일 형식이 유효하지 않습니다.'
+            ],
+            nicknameRules: [
+                v => !!v || '닉네임은 필수입니다.'
+            ],
+            passwordRules: [
+                v => !!v || '비밀번호는 필수입니다.'
+            ],
+            passwordCheckRules: [
+                v => !!v || '비밀번호를 확인해 주십시오!',
+                v => v === this.password || '비밀번호가 일치하지 않습니다.'
+            ]
+        }
+    },
+    methods: {
+        onSubmitForm() {
+            /* 
+                vuetify가 제공하는 기능이다.
+                v-form 태그에 ref를 명시하고,
+                내부에 있는 입력 폼들의 rules가 모두 만족 되면,
+                this.valid를 true로,
+                아니면 false로 자동 셋팅해준다.
+            */
+            if (this.$refs.form.validate()) {
+                alert('회원가입 form submit 시도!!')
+            } else {
+                alert('폼이 유효하지 않습니다.')
+            }
         }
     },
     head() {
