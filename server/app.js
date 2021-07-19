be964960f6735375fcdd43b1d62a54eb2d5a2ca5
kyomin <kim031504@naver.com>
2021-07-19 18:36:39 +0900

    이메일 중복 체크와 모델(db table) 수정

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const app = express()
 
 /* database connection */
 const db = require('./models')
-db.sequelize.sync()
+db.sequelize.sync({ force: true })  // 모델(table) 구조 수정 시 강제 적용. 테이블 지웠다가 다시 만듦.
 
 /* express 미들웨어 등록 */
 app.use(express.json())
@@ -20,6 +20,17 @@ app.get('/', (req, res) => {
 
 app.post('/user', async (req, res, next) => {
     try {
+        // 가입 이메일 중복 검사
+        const exUser = await db.User.findOne({
+            email: req.body.email
+        })
+        if (exUser) {
+            return res.status(403).json({
+                errorCode: 1,
+                message: '이미 회원가입 되어있습니다.'
+            })
+        }
+
         const hash = await bcrypt.hash(req.body.password, 12)   // 비밀번호 암호화
         const newUser = await db.User.create({
             email: req.body.email,
@@ -27,10 +38,10 @@ app.post('/user', async (req, res, next) => {
             nickname: req.body.nickname
         })
 
-        res.status(201).json(newUser)
+        return res.status(201).json(newUser)
     } catch (err) {
         console.error(err)
-        next(err)
+        return next(err)    // next에 인자 담으면 에러로 응답
     }
 })
 
--- server/models/user.js
+++ server/models/user.js
@@ -8,7 +8,8 @@ module.exports = (sequelize, DataTypes) => {
     const User = sequelize.define('User', {
         email: {
             type: DataTypes.STRING(40), // 40자 이내
-            allowNull: false    // 필수
+            allowNull: false,    // 필수
+            unique: true    // 중복 금지
         },
         nickname: {
             type: DataTypes.STRING(20),
